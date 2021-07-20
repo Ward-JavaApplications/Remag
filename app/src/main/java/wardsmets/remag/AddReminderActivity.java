86250f9646a57021bf04c67afe09e289bbb56896
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2021-07-20 21:24:26 +0200

    tried some but can't view recyclerView
    https://github.com/android/views-widgets-samples/tree/main/RecyclerView/
    
    https://stackoverflow.com/questions/40584424/simple-android-recyclerview-example
    
    https://www.youtube.com/watch?v=Wq2o4EbM74k

--- app/src/main/java/wardsmets/remag/AddReminderActivity.java
+++ app/src/main/java/wardsmets/remag/AddReminderActivity.java
@@ -1,15 +1,42 @@
 package wardsmets.remag;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class AddReminderActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import wardsmets.remag.RecyclerView.MyRecyclerViewAdapter;
+
+public class AddReminderActivity extends AppCompatActivity{
+
+    private ArrayList<String> times;
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+        times = new ArrayList<>();
+        times.add("morgen");
+        times.add("gister");
+        setUpRecycleView();
+    }
+
+    private void setUpRecycleView(){
+        try {
+            RecyclerView recyclerView = findViewById(R.id.recycler_view_times);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            MyRecyclerViewAdapter recyclerViewAdapter = new MyRecyclerViewAdapter(times);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
